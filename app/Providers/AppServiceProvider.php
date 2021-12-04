694acb6862d4105ec9a5e79566dd0e9f0ff47669
kitamura <s.kitamura.biz@gmail.com>
2021-12-04 12:52:09 +0900

    APP_ENV=ngrokでassetの出力https対応

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,8 +21,11 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(\Illuminate\Routing\UrlGenerator $url)
     {
-        //
+        if(config('app.env') === 'ngrok')
+        {
+            $url->forceScheme('https');
+        }
     }
 }
